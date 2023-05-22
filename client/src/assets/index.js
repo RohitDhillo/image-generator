588df433f4b677638076974493dda12626ad7a52
RohitDhillo <rohitdhillon211@gmail.com>
2023-05-22 11:07:27 +0530

    first commit

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,9 +1,11 @@
 import download from './download.png';
 import logo from './logo.svg';
+import myLogo from './logo.png';
 import preview from './preview.png';
 
 export {
   download,
   logo,
   preview,
+  myLogo,
 };
